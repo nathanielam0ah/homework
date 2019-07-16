33866a3cdba4f5e24ec107d798c7e178732f9888
nathanielam0ah <nkp.amoah@gmail.com>
2019-07-16 20:06:36 +0000

    new labs

---
+++ day02/labs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+python3 = "Python3 is the future"
+print(python3)
+
+python2 = 'Python2 is legacy'
+print(python2)
+
+multiple_line_string = '''
+                        I scream,
+                        you scream,
+                        we all scream ICE CREAM!
+                        '''
+print(multiple_line_string)
+
+muliple_line_string02 = "I am" + " learning Python3"
+print(muliple_line_string02)
+
+length_string = "Hello Python"
+print(len(length_string))
+
+one_string = "Hello, " + "World!"
+print(one_string)
