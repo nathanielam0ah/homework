cd0e0feed6f02f8c420563892d5ae6cc93a78c11
nathanielam0ah <nkp.amoah@gmail.com>
2019-07-16 20:51:20 +0000

    added answers for question VII

--- day02/labs.py
+++ day02/labs.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-python3 = "Python3 is the future"
+python3 = "Python3 is the future."
 print(python3)
 
-python2 = 'Python2 is legacy'
+python2 = 'Python2 is legacy.'
 print(python2)
 
 multiple_line_string = '''
@@ -13,11 +13,18 @@ multiple_line_string = '''
                         '''
 print(multiple_line_string)
 
-muliple_line_string02 = "I am" + " learning Python3"
+muliple_line_string02 = "I am" + " learning Python3."
 print(muliple_line_string02)
 
 length_string = "Hello Python"
 print(len(length_string))
 
-one_string = "Hello, " + "World!"
-print(one_string)
+hello_string = "Hello, " + "World!"
+print(hello_string)
+
+one_string = "This is python."
+two_string = "I like Python."
+print(one_string, two_string)
+
+zing_string = "bazinga"
+print(zing_string[2:6])
