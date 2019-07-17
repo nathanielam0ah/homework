bb6e8b9d77e72108e62189d35abd98256a6e5adb
nathanielam0ah <nkp.amoah@gmail.com>
2019-07-17 16:57:46 +0000

    assingment for day03

---
+++ day03/lab2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+account01 = {"Nathaniel", "Amoah", "Savings", 500.00}
+account02 = {"Kofi" , "Addae", "Current", 300.00}
+account03 = {"Kwame","Peprah","Investment",20.00}
+
+b_accts=["001":account01, "002":account02, "003":account03]
+print(b_accts)
+ 
