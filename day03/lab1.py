6f8e7c653233c06d6cb521e7b395235e8d6f6a2d
nathanielam0ah <nkp.amoah@gmail.com>
2019-07-17 18:07:46 +0000

    complete

--- day03/lab1.py
+++ day03/lab1.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python
 
-account01 = ["Nathaniel", "Amoah", "Savings", 500.00]
-account02 = ["Kofi" , "Addae", "Current", 300.00]
-account03 = ["Kwame","Peprah","Investment",20.00]
+account01 = {"firstname":"Nathaniel", "lastname":"Amoah", "accountType":"Savings", "balance":500.00}
+balance01 = account01.get("balance")
+account02 = {"firstname":"James", "lastname":"Gunn", "accountType":"Loans", "balance":500.00}
+balance02 = account02.get("balance")
+account03 = {"firstname":"Hanzou", "lastname":"Hasashi", "accountType":"Investment", "balance":500.00}
+balance03 = account03.get("balance")
 
-b_accts={"001":account01, "002":account02, "003":account03}
-print(b_accts)
+bankAccounts = [account01, account02, account03]
+print(bankAccounts)
 
+def deposit(balance,amountToDeposit):
+	return balance+amountToDeposit
+def withdraw(balance,amountToWithdraw):
+	return balance - amountToWithdraw
 
+acc_01 = deposit(balance01,5645)
+acc_001 = withdraw(balance01,123)
+print(acc_01)
+print(acc_001)
+
+acc_02 = deposit(balance02,789)
+acc_002 = withdraw(balance02,132)
+print(acc_02)
+print(acc_002)
+
+acc_03 = deposit(balance03,200)
+acc_003 = withdraw(balance03,300)
+print(acc_03)
+print(acc_003)
+ 
--- day03/lab3.py
+++ day03/lab3.py
@@ -1,9 +1,32 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-account01 = {"Nathaniel", "Amoah", "Savings", 500.00}
-account02 = {"Kofi" , "Addae", "Current", 300.00}
-account03 = {"Kwame","Peprah","Investment",20.00}
+account01 = {"firstname":"Nathaniel", "lastname":"Amoah", "accountType":"Savings", "balance":500.00}
+balance01 = account01.get("balance")
+account02 = {"firstname":"James", "lastname":"Gunn", "accountType":"Loans", "balance":500.00}
+balance02 = account02.get("balance")
+account03 = {"firstname":"Hanzou", "lastname":"Hasashi", "accountType":"Investment", "balance":500.00}
+balance03 = account03.get("balance")
 
-b_accts={"001":account01, "002":account02, "003":account03}
-print(b_accts)
+bank_accounts={"001":"account01", "002":"account02", "003":"account03"}
+print(bank_accounts)
 
+def deposit(balance,amountToDeposit):
+	return balance+amountToDeposit
+def withdraw(balance,amountToWithdraw):
+	return balance - amountToWithdraw
+
+acc_01 = deposit(balance01,5234)
+acc_001 = withdraw(balance01,3346)
+print(acc_01)
+print(acc_001)
+
+acc_02 = deposit(balance02,47457)
+acc_002 = withdraw(balance02,856)
+print(acc_02)
+print(acc_002)
+
+acc_03 = deposit(balance03,689)
+acc_003 = withdraw(balance03,989)
+print(acc_03)
+print(acc_003)
+ 
