0a13f10108de7010832c226426095b4c009f5e68
nathanielam0ah <nkp.amoah@gmail.com>
2019-07-18 22:46:45 +0000

    Completed Assignment

--- squares.py
+++ squares.py
@@ -4,9 +4,9 @@ def sqr(the_number):
 	return the_number * the_number
 
 init = [1, 2 , 3, 4 , 5, 6, 7, 8, 9, 10,]
-init_Two = []
+init_two = []
 
 for x in init:
-	init_Two.append( sqr(x) )
+	init_two.append( sqr(x) )
 
-print(init_Two)
+print(init_two)
