3477ed7186080df93e4abe49d36bd87d9e97315b
nathanielam0ah <nkp.amoah@gmail.com>
2019-07-22 23:54:48 +0000

    ..!

--- squares.py
+++ squares.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
 def sqr(the_number):
-	return the_number * the_number
 
 init = [1, 2 , 3, 4 , 5, 6, 7, 8, 9, 10,]
 init_two = []
-
 for x in init:
 	init_two.append( sqr(x) )
+return the_number * the_number
 
 print(init_two)
