fda16308f614fe65dd21923afe3defebf16491c4
nathanielam0ah <nkp.amoah@gmail.com>
2019-07-23 02:10:28 +0000

    improved readability

--- day06/groceryList.py
+++ day06/groceryList.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
 def groceryList():
-    menuItem = ""
     myList = []
-<<<<<<< HEAD
     menuItem = None
     userinputItem = None
     while (menuItem != ":q"):
@@ -18,22 +16,6 @@ def groceryList():
         if (menuItem == ":v"):
             print(myList)
         if (menuItem) == ":q":
-=======
-    userinputItem = ""
-    while (menuItem != "exit"):
-        print("grocery list console v1./n enter :edit to edit your list :view to read your list and :exit to close")
-        menuItem = input()
-        if (menuItem == "edit"):
-            print("hit enter to move to a new line and :q to exit")
-            while (userinputItem != ":q"):
-                userinputItem = input()
-                myList.append(userinputItem)
-                if (userinputItem == ":q"):
-                    break
-        if (menuItem == ":view"):
-            print(myList)
-        elif menuItem == ":exit":
->>>>>>> 2418a3c8208e083267afcaae825db8f38c75b4ff
             exit()
 
 groceryList()
