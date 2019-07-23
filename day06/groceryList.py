99ba3a1f79128271c8c17b1c4de4ee9d123e5865
nathanielam0ah <nkp.amoah@gmail.com>
2019-07-23 01:06:24 +0000

    added menu items but has errors.

--- day06/groceryList.py
+++ day06/groceryList.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 
 def groceryList():
+    print("grocery list console v1./n enter :edit to edit your list :view to read your list and :exit to close")
+    menuItem = input()
     myList = []
     userinputItem = ""
-    while (userinputItem != ":x"):
-        userinputItem = input()
-        myList.append(userinputItem)
-        if userinputItem == ":exit":
+    while (menuItem != "exit"):
+        if (menuItem == "edit"):
+            print("hit enter to move to a new line and :q to exit")
+            while (userinputItem != ":q"):
+                userinputItem = input()
+                myList.append(userinputItem)
+                if (userinputItem == ":q"):
+                    break
+        if (menuItem == ":view"):
+            print(myList)
+            break
+        elif menuItem == ":exit":
             exit()
 
 groceryList()
